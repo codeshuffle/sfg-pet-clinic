c5073c79bac143e313cfe541346135238af3c645
codeshuffle <bogdan.balasanu@gmail.com>
2020-06-21 13:35:18 -0700

    Refactoring some service interfaces. CLoses #4

--- pet-clinic-data/src/main/java/bb/org/petclinic/service/VetService.java
+++ pet-clinic-data/src/main/java/bb/org/petclinic/service/VetService.java
@@ -1,12 +1,9 @@
 package bb.org.petclinic.service;
 
-import bb.org.petclinic.model.Pet;
 import bb.org.petclinic.model.Vet;
 
 import java.util.Set;
 
-public interface VetService {
-    Vet findById(Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
+public interface VetService extends CrudService<Vet, Long>{
+
 }
