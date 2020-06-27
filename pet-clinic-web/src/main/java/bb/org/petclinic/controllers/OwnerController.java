8b13cb2b66354accc73f48d6c168de6148ab47a8
codeshuffle <bogdan.balasanu@gmail.com>
2020-06-27 14:06:14 -0700

    List owners. Thymeleaf template each. Model model injection. Closes #10

--- pet-clinic-web/src/main/java/bb/org/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/bb/org/petclinic/controllers/OwnerController.java
@@ -1,14 +1,28 @@
 package bb.org.petclinic.controllers;
 
+import bb.org.petclinic.model.Owner;
+import bb.org.petclinic.service.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Set;
 
 @Controller
 @RequestMapping("/owner")
 public class OwnerController {
 
+    private Set<Owner> owners;
+    private final OwnerService ownerService;
+
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+
     @RequestMapping({"","/", "/index", "/index.html"})
-    public String listOwners() {
+    public String listOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
