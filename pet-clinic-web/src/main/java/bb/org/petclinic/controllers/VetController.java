e29a62d496d7263be44bab7eb7544a5e2081ab5a
codeshuffle <bogdan.balasanu@gmail.com>
2020-06-27 14:18:46 -0700

    List vets. Thymeleaf template each. Model model injection. Closes #12

--- pet-clinic-web/src/main/java/bb/org/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/bb/org/petclinic/controllers/VetController.java
@@ -1,13 +1,23 @@
 package bb.org.petclinic.controllers;
 
+import bb.org.petclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index"})
-    public String listVets() {
+    public String listVets(Model model)
+    {
+        model.addAttribute("vets", vetService.findAll());
         return  "vet/index";
     }
 }
