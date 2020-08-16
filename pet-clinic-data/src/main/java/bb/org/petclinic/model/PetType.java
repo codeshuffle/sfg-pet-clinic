e30fb9d760f5af94c1f2a97a45f04ff706c1e77d
codeshuffle <bogdan.balasanu@gmail.com>
2020-08-16 14:54:16 -0700

    end of Lesson 167

--- pet-clinic-data/src/main/java/bb/org/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/bb/org/petclinic/model/PetType.java
@@ -1,9 +1,16 @@
 package bb.org.petclinic.model;
 
+import lombok.*;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity{
@@ -11,12 +18,6 @@ public class PetType extends BaseEntity{
     @Column(name = "name")
     private String name;
 
-    public String getName() {
-        return name;
-    }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
 
--- pet-clinic-data/src/main/java/bb/org/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/bb/org/petclinic/model/Vet.java
@@ -1,11 +1,17 @@
 package bb.org.petclinic.model;
 
+import lombok.*;
 import org.hibernate.annotations.ManyToAny;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
@@ -15,19 +21,5 @@ public class Vet extends Person {
             inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities= new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
 
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
-
-    @Override
-    public String toString() {
-        return "Vet{" +
-                "name=" + super.getFirstName() +" : "+ super.getFirstName()+
-                " specialities=" + specialities +
-                '}';
-    }
 }
