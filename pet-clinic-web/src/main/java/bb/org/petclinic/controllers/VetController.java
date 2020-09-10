48c88a72270608fd0e572ec58880e793a89381c6
codeshuffle <bogdan.balasanu@gmail.com>
2020-09-11 01:17:14 +0300

    Vet Controllers and JSON Endpoint. L245

--- pet-clinic-web/src/main/java/bb/org/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/bb/org/petclinic/controllers/VetController.java
@@ -6,7 +6,9 @@ import bb.org.petclinic.service.VetService;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Set;
 
@@ -31,5 +33,11 @@ public class VetController {
     {
         model.addAttribute("vets", vetService.findAll());
         return  "vets/index";
+    }
+
+    @RequestMapping("/api/vets")
+    public @ResponseBody  Set<Vet>  getVetsJson() {
+
+        return vetService.findAll();
     }
 }
